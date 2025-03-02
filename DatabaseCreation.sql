d8721e4ac4b3e86a280f4e631bdaeadce003be10
ParnanenJ <jiro.parnanen@student.laurea.fi>
2025-03-02 18:57:52 +0200

    Tables created and testet

---
+++ DatabaseCreation.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Sijainti ( 
+    SijaintiID int NOT NULL, 
+    Kaupunki varchar(30) NOT NULL,
+    Maa varchar(30) NOT NULL,  
+    PRIMARY KEY (SijaintiID)
+);
+
+CREATE TABLE Myyja ( 
+    MyyjaID int NOT NULL, 
+    SijaintiID int NOT NULL, 
+    Etunimi varchar(30) NOT NULL,
+    Sukunimi varchar(30) NOT NULL,
+    SPosti varchar(40) NOT NULL, 
+    PuhNro varchar(15) NOT NULL, 
+    PRIMARY KEY (MyyjaID), 
+    FOREIGN KEY (SijaintiID) REFERENCES Sijainti(SijaintiID)
+);
+
+CREATE TABLE Auto ( 
+    RekNro varchar(7) NOT NULL, 
+    Merkki varchar(20) NOT NULL,
+    Malli varchar(20) NOT NULL, 
+    Vapaa TINYINT(1) NOT NULL,
+    SijaintiID int NOT NULL,
+    MyyjaID int NOT NULL,  
+    PRIMARY KEY (RekNro), 
+    FOREIGN KEY (SijaintiID) REFERENCES Sijainti(SijaintiID), 
+    FOREIGN KEY (MyyjaID) REFERENCES Myyja(MyyjaID)
+);
+
+CREATE TABLE Asiakas ( 
+    AsiakasID int NOT NULL, 
+    Etunimi varchar(30) NOT NULL,
+    Sukunimi varchar(30) NOT NULL, 
+    SPosti varchar(40) NOT NULL, 
+    PuhNro varchar(15) NOT NULL, 
+    PRIMARY KEY (AsiakasID)
+);
+
+CREATE TABLE Varaus ( 
+    VarausID int NOT NULL, 
+    AsiakasID int NOT NULL,  
+    RekNro varchar(7) NOT NULL,
+    StartTime DATETIME NOT NULL,
+    EndTime DATETIME NOT NULL,
+    PRIMARY KEY (VarausID), 
+    FOREIGN KEY (AsiakasID) REFERENCES Asiakas(AsiakasID),
+    FOREIGN KEY (RekNro) REFERENCES Auto(RekNro)
+);
+
